055b7b057ba5075b241072e7ab7014a43ad8cbee
Bùi Hải Triều <trieuql159@gmail.com>
2023-07-26 23:02:36 +0700

    initial project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+
+    /* Build should comment server */
+    server: {
+        host: '127.0.0.1',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'https://api-training.hrm.div4.pgtest.co',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
+                // /api/v1
+                secure: false,
+            },
+        },
+    },
+});

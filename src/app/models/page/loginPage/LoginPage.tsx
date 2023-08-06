3e810de1d4f38d0edb149e872f2e8c8db9c52f55
Bùi Hải Triều <trieuql159@gmail.com>
2023-08-06 22:22:12 +0700

    "Login Form"

--- src/app/models/page/loginPage/LoginPage.tsx
+++ src/app/models/page/loginPage/LoginPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import LoginForm from '../../auth/loginForm/LoginForm';
 
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+    return (
+        <div style={{ height: '100vh' }} className="flex mx-auto items-center justify-center bg-[#fafafa]">
+            <LoginForm />
+            {/* {contextHolder} */}
+        </div>
+    );
 };
 
 export default LoginPage;

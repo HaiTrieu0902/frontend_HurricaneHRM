27808e92d851b8a0a617b846d06378248a79de81
Bùi Hải Triều <trieuql159@gmail.com>
2023-08-08 13:27:30 +0700

    "Header decor"

--- src/app/layout/header/Header.tsx
+++ src/app/layout/header/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Header.scss';
-import { Col, Image, Row } from 'antd';
+import { Col, Image, Row, Select } from 'antd';
+import AvatarDefault from '../../../assets/img/avatardefault.jpg';
 import Logo from '../../../assets/logo.svg';
-
+import { ExpandAltOutlined } from '@ant-design/icons';
 const Header = () => {
+    const handleChange = (value: string) => {
+        console.log(`selected ${value}`);
+    };
     return (
         <Row align={'middle'}>
             <Col span={12}>
@@ -20,9 +24,42 @@ const Header = () => {
                 </Row>
             </Col>
             <Col className="middle_content" span={12}>
-                <Row justify={'end'}>
+                <Row style={{ display: 'flex', alignItems: 'center' }} gutter={[12, 12]} justify={'end'}>
+                    <Col style={{ marginRight: '12px' }}>
+                        <Select
+                            defaultValue="light"
+                            style={{ width: 120, color: '#035397' }}
+                            onChange={handleChange}
+                            options={[
+                                { value: 'light', label: 'Dark' },
+                                { value: 'dark', label: 'Light' },
+                            ]}
+                        />
+                    </Col>
+                    <Col style={{ marginRight: '12px' }}>
+                        <Select
+                            defaultValue="en"
+                            style={{ width: 120, color: '#035397' }}
+                            onChange={handleChange}
+                            options={[
+                                { value: 'en', label: 'English' },
+                                { value: 'vn', label: 'Vietnamese' },
+                                { value: 'japan', label: 'Japanese' },
+                                { value: 'china', label: 'China', disabled: true },
+                            ]}
+                        />
+                    </Col>
+
                     <Col>
-                        <div>hhii</div>
+                        <div className="middle-image_user">
+                            <Image className="image_user" src={AvatarDefault} preview={false}></Image>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="midle_position">
+                            <p className="-mt-3 font-semibold text-sm">User name</p>
+                            <span className="midle-sub_position">Position</span>
+                        </div>
                     </Col>
                 </Row>
             </Col>
